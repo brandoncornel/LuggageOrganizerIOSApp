11fbd80f190da5099436604513e779cc731f4691
bcc4679 <bcc4679@mail.rit.edu>
2017-09-23 11:03:10 -0400

    Initial Commit

---
+++ LuggageOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuggageOrganizer
+//
+//  Created by Brandon Cornel on 9/23/17.
+//  Copyright © 2017 Brandon Cornel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
